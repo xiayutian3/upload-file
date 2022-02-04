e2b2f019e950fb91a8bcdcecc4ffa9aa00b1d8df
1127071993@qq.com <aini2009626>
2022-02-04 15:57:22 +0800

    p3:上传文件大小或者格式等限制的处理

---
+++ src/js/instance.js
@@ -0,0 +1,23 @@
+/**
+ * axios 请求的公共信息进行提取
+ */
+ let instance = axios.create();
+ instance.defaults.baseURL = 'http://127.0.0.1:8888'
+ instance.defaults.headers['Content-Type'] = 'multipart/form-data'
+ instance.defaults.transformRequest = (data, headers) => {
+     const contentType = headers['Content-Type']
+     if (contentType === 'application/x-www-form-urlencoded') {
+         return Qs.stringify(data) // 传递参数格式
+     }
+     return data    
+ }
+ 
+ // 响应拦截器
+ instance.interceptors.response.use( response => {
+     return response.data
+ }, reason => {
+     // 请求失败统一处理
+     return Promise.reject(reason)
+ })
+ 
+ 
